43855abf56699e04c5b0c146558c4dafd19f4a62
shoaib12k <shoaib1207k@gmail.com>
2022-05-16 03:22:56 +0530

    tested classes

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -1,8 +1,26 @@
 package com.dsa;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FizzBuzz fb = new FizzBuzz();
+        System.out.println(fb.fizzBuzz(15));
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,15};
+        int sum = 18;
+
+        Pairs p = new Pairs();
+        int[] hashPair = p.sumPairHash(arr,sum);
+        int[] setPair = p.sumPairSet(arr,sum);
+        System.out.println(hashPair[0]+","+hashPair[1]);
+        System.out.println(setPair[0]+","+setPair[1]);
+
+        Triplets t = new Triplets();
+        ArrayList<ArrayList<Integer>> trips = t.findTriplets(arr,sum);
+        System.out.println(trips);
+
+
     }
 }
