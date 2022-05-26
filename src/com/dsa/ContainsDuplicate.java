471664b534ef6faeada7cceba064a229f0f524c6
shoaib12k <shoaib1207k@gmail.com>
2022-05-26 20:40:39 +0530

    Contains Duplicate

--- src/com/dsa/ContainsDuplicate.java
+++ src/com/dsa/ContainsDuplicate.java
@@ -5,14 +5,12 @@ import java.util.HashSet;
 public class ContainsDuplicate {
     public boolean isDuplicate(int[] arr){
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<arr.length;i++){
-            if(set.contains(arr[i])){
+        for(int i=0;i<arr.length;i++)
+            if (set.contains(arr[i])) {
                 return true;
-            }
-            else{
+            } else {
                 set.add(arr[i]);
             }
-        }
         return false;
     }
 }
